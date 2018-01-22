ce2a4719979686185b982953713c8929d8ab34ea
Danny <contact@danfoster.io>
2018-01-21 19:19:24 -0600

    URL

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^', views.index),
-    url(r'^shop/checkout$', views.checkout),
+    url(r'^checkout$', views.checkout),
     url(r'^success$', views.success)
 ]
